92de6e617f62678b814c3ce617429f0c3a6fb5c9
Ofri Harlev <ofri.harlev@gmail.com>
2017-01-14 02:36:59 -0800

    Rank gets posted for specific user

--- hackUCI/src/pages/about/about.ts
+++ hackUCI/src/pages/about/about.ts
@@ -11,19 +11,21 @@ export class AboutPage {
 
   rating:any
   item:any
+  userID:any
   constructor(public navCtrl: NavController, public af: AngularFire) {
   }
 
   Rating(){
     const itemObservable = this.af.database.object('/item');
-    console.log(itemObservable);
-    console.log(this.rating);
-    this.item = this.af.database.object('/item', { preserveSnapshot: true });
+    //console.log(itemObservable);
+    //console.log(this.rating);
+    this.userID = this.af.auth.getAuth().uid;
+    this.item = this.af.database.object('/Classes/1/Students/'+this.userID+'/rating', { preserveSnapshot: true });
     this.item.set(this.rating);
-    this.item.subscribe(snapshot => {
-      console.log(snapshot.key)
-      console.log(snapshot.val())
-    });
+    //this.item.subscribe(snapshot => {
+    //  console.log(snapshot.key)
+    //  console.log(snapshot.val())
+    //});
   }
 
 }
