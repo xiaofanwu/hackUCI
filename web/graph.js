6d12b03565a0809f23a38add0fd5fd6d9284f2c0
Timothy Johnson <timjohnson314@gmail.com>
2017-01-13 23:41:55 -0800

    Scrolling graph

--- web/graph.js
+++ web/graph.js
@@ -1,30 +1,28 @@
-function setupGraph() {
+function drawGraph() {
 	// Set the dimensions of the canvas / graph
 	var margin = {top: 30, right: 20, bottom: 30, left: 50},
 	    width = 600 - margin.left - margin.right,
 	    height = 270 - margin.top - margin.bottom;
-
-	// Parse the date / time
-	var parseDate = d3.time.format("%d-%b-%y").parse;
+	window.height = height;
 
 	// Set the ranges
-	var x = d3.time.scale().range([0, width]);
-	var y = d3.scale.linear().range([height, 0]);
+	window.x = d3.time.scale().range([0, width]);
+	window.y = d3.scale.linear().range([height, 0]);
 
 	// Define the axes
-	var xAxis = d3.svg.axis().scale(x)
+	window.xAxis = d3.svg.axis().scale(x)
 	    .orient("bottom").ticks(5);
 
-	var yAxis = d3.svg.axis().scale(y)
+	window.yAxis = d3.svg.axis().scale(y)
 	    .orient("left").ticks(5);
 
 	// Define the line
-	var valueline = d3.svg.line()
+	window.valueline = d3.svg.line()
 	    .x(function(d) { return x(d.time); })
 	    .y(function(d) { return y(d.val); });
     
 	// Adds the svg canvas
-	var svg = d3.select("body")
+	window.svg = d3.select("body")
 	    .append("svg")
 	        .attr("width", width + margin.left + margin.right)
 	        .attr("height", height + margin.top + margin.bottom)
@@ -32,24 +30,35 @@ function setupGraph() {
 	        .attr("transform", 
 				"translate(" + margin.left + "," + margin.top + ")");
 
-	var data = [];
+	window.data = [];
+	window.currTime = 0;
 	var initSize = 100;
-	var currTime = 0;
 	for (var i = 0; i < initSize; i++) {
-		data[i] = {time: currTime, val: Math.random()};
+		window.data[i] = {time: currTime, val: Math.random()};
 		currTime += 1;
 	}
+	
+	// console.log(window.data);
+	
+	window.setInterval(updateAndDraw, 1000);
+}
 
+function updateAndDraw() {
+	// Update
+	svg.selectAll("*").remove();
+	window.data.shift();
+	window.data[window.data.length] = {time: currTime, val: Math.random()};
+	window.currTime += 1;
+	
+	// console.log(data);
     // Scale the range of the data
-    x.domain(d3.extent(data, function(d) { return d.time; }));
-    y.domain([0, d3.max(data, function(d) { return d.val; })]);
+    x.domain(d3.extent(window.data, function(d) { return d.time; }));
+    y.domain([0, d3.max(window.data, function(d) { return d.val; })]);
 
     // Add the valueline path.
     svg.append("path")
         .attr("class", "line")
         .attr("d", valueline(data));
-		
-	print("Data: " + valueline(data));
 
     // Add the X Axis
     svg.append("g")
