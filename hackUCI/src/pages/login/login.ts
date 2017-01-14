66280f2d851ff17c495d70a3ad9a1786165fa433
xiaofanwu <wuxiaofan1996@gmail.com>
2017-01-14 01:36:33 -0800

    added login page with firebase

--- hackUCI/src/pages/login/login.ts
+++ hackUCI/src/pages/login/login.ts
@@ -1,15 +1,45 @@
 import { Component } from '@angular/core';
 import {AngularFire, AuthProviders, AuthMethods} from 'angularfire2';
 import { NavController } from 'ionic-angular';
+import { RegisterPage } from '../register/register';
+import { TabsPage } from '../pages/tabs/tabs';
 
 @Component({
   selector: 'page-login',
   templateUrl: 'login.html'
 })
 export class LoginPage {
+  registerCredentials = {email: '', password: ''};
 
-  constructor(public navCtrl: NavController) {
+
+  constructor(public navCtrl: NavController, public af: AngularFire) {
 
   }
+
+
+ 	public login(){
+ 		this.af.auth.login({
+		  email: 'wuxiaofan1996@gmail.com',
+		  password: 'testdfddfdf',
+		},
+		{
+		  provider: AuthProviders.Password,
+		  method: AuthMethods.Password,
+		});
+		this.navCtrl.push(TabsPage);
+
+
+  	}
+ 
+  	public register(){
+  	this.navCtrl.push(RegisterPage);
+ 	console.log("came to logi");
+ 	console.log(this.registerCredentials);
+	 	var email = "wuxiaofan1996@gmail.com";
+	 	var password = "testdfddfdf";
+
+	  this.af.auth.createUser(this.registerCredentials);
+
+  	}
 
 }
---
+++ hackUCI/src/pages/register/register.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+/*
+  Generated class for the Register page.
+
+  See http://ionicframework.com/docs/v2/components/#navigation for more info on
+  Ionic pages and navigation.
+*/
+@Component({
+  selector: 'page-register',
+  templateUrl: 'register.html'
+})
+export class RegisterPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams,public af: AngularFire) {}
+
+    public register(){
+	 	console.log("came to logi");
+	 	console.log(this.registerCredentials);
+		 	var email = "wuxiaofan1996@gmail.com";
+		 	var password = "testdfddfdf";
+
+		  this.af.auth.createUser(this.registerCredentials);
+
+  	}
+
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad RegisterPage');
+  }
+
+}
