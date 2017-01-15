f1837d32340fe9961203d008a4f8f23c029a3aad
xiaofanwu <wuxiaofan1996@gmail.com>
2017-01-14 16:26:11 -0800

    Class enrolled works and fixed css

--- hackUCI/src/pages/tabs/tabs.ts
+++ hackUCI/src/pages/tabs/tabs.ts
@@ -16,13 +16,53 @@ export class TabsPage {
   // tab3Root: any = ContactPage;
   userID: any
   classes: FirebaseListObservable<any>;
-  enroll: any; 
+  classesAvailable: FirebaseListObservable<any>;
+  enroll: any;
+  selected:String;
+  classesCanEnroll: string[] = [];
   courseCode = {cc:''}
+  classEnrolled:string[] = [];
 
   constructor(public navCtrl: NavController, public af: AngularFire) {
   	this.classes = af.database.list('/Users/' + this.af.auth.getAuth().uid + '/Enrolled');
-  	console.log(this.classes)
+    this.classesAvailable = af.database.list('/Classes');
+
+    af.database.list('/Users/' + this.af.auth.getAuth().uid + '/Enrolled', { preserveSnapshot: true})
+    .subscribe(snapshots=>{
+        snapshots.forEach(snapshot => {
+          this.classEnrolled.push(snapshot.val());
+         
+        });
+    })
+    console.log(this.classEnrolled,"classava enrolled******");
+
+    this.classesCanEnroll=[];
+    af.database.list('/Classes', { preserveSnapshot: true})
+    .subscribe(snapshots=>{
+        snapshots.forEach(snapshot => {
+          //if the class is already enrolled, do nothing
+          if (this.classEnrolled.indexOf(snapshot.key)==-1){
+            this.classesCanEnroll.push(snapshot.key);
+          }
+        });
+    })
+    console.log(this.classesCanEnroll);
+    console.log("after this********");
+
+    // for (let entry of this.classes) {
+    // console.log(entry); // 1, "string", false
+    // }
+
   }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad ***********');
+  }
+
+  showSelectValue(mySelect) {
+    this.selected = mySelect;
+  }
+
 
   classSelect(cid:any) {
   	this.navCtrl.push(AboutPage, {
@@ -32,8 +72,15 @@ export class TabsPage {
 
   classEnroll(cid:any) {
   	this.userID = this.af.auth.getAuth().uid;
-    this.enroll = this.af.database.list('/Users/' + this.userID + '/Enrolled/').push('new enrollment');
+    this.enroll = this.af.database.list('/Users/' + this.userID + '/Enrolled');
+
+    // this.enroll = this.af.database.list('/Users/' + this.userID + '/Enrolled/').push('new enrollment');
     //console.log(this.courseCode.cc);
-    this.enroll.set(this.courseCode.cc);
+    this.enroll.push(this.selected);
+
+    // this.enroll.set(this.selected);
+    this.navCtrl.push(AboutPage, {
+      cid:this.selected
+    });
   }
 }
